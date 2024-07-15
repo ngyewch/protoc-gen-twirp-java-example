cbffc7dce016468fac9fab862188053874bf61c2
Nick Ng <ngyewch@gmail.com>
2024-07-15 13:01:19 +0800

    chore: updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     application
     java
-    id("com.autonomousapps.dependency-analysis") version "1.28.0"
-    id("com.diffplug.spotless") version "6.23.3"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.autonomousapps.dependency-analysis") version "1.32.0"
+    id("com.diffplug.spotless") version "6.25.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
     id("com.google.protobuf") version "0.9.4"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
 }
@@ -21,14 +21,14 @@ dependencies {
 
     implementation("com.google.protobuf:protobuf-java:3.25.1")
     implementation("com.google.protobuf:protobuf-java-util:3.25.1")
-    implementation("info.picocli:picocli:4.7.5")
+    implementation("info.picocli:picocli:4.7.6")
     implementation("io.helidon.common:helidon-common-http")
     implementation("io.helidon.common:helidon-common-reactive")
     implementation("io.helidon.media:helidon-media-common")
     implementation("io.helidon.webclient:helidon-webclient")
     implementation("io.helidon.webserver:helidon-webserver")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
